b7af62befa908a4705323e11878a9a6ebda28525
太田京伽 <kyokaota@ootamiyakotoginoMacBook-Pro.local>
2022-04-12 18:49:02 +0900

    modify

--- js/top.js
+++ js/top.js
@@ -1,3 +1,15 @@
 $(function() {
     $('html,body').animate({ scrollTop: 0 }, '1');
+
+
+    $('.slick01').slick({
+        arrows: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '25%',
+        focusOnSelect: true,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>'
+        
+    });
 });
