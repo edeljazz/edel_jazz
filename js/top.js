160e31cfb006a3fbf74e8a17bde6c670d85a0d67
太田京伽 <kyokaota@ootamiyakotoginoMacBook-Pro.local>
2022-04-13 17:50:31 +0900

    modify

--- js/top.js
+++ js/top.js
@@ -1,35 +1,19 @@
 $(function() {
     $('html,body').animate({ scrollTop: 0 }, '1');
-    /*$('.slick01').slick({
-        dots: true,
-        arrows: true,
-        centerMode: true,
-        centerPadding: '15%',
-        focusOnSelect: true,
-        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-        nextArrow: '<button class="slide-arrow next-arrow"></button>',
-    }); */
     $('.slick01').slick({
       dots: true,
       arrows: true,
       centerMode: true,
       centerPadding: '25%',
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
       responsive: [
         {
-          breakpoint: 800,
+          breakpoint: 499,
           settings: {
-          centerMode: true,
           centerPadding: '10%',
-          slidesToShow: 1,
-          slidesToScroll: 1,
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
 });
