ca1d8be2946164c8728adaba6a246b8fa41773db
太田京伽 <kyokaota@ootamiyakotoginoMacBook-Pro.local>
2022-04-15 00:56:20 +0900

    modify information style

--- js/top.js
+++ js/top.js
@@ -2,7 +2,7 @@ $(function() {
     $('html,body').animate({ scrollTop: 0 }, '1');
     $('.slick01').slick({
       dots: true,
-      arrows: true,
+      arrows: false,
       centerMode: true,
       centerPadding: '25%',
       responsive: [
@@ -14,4 +14,16 @@ $(function() {
         }
       ]
     });
+    var show = 8; //最初に表示する件数
+    var num = 6;  //clickごとに表示したい件数
+    var contents = '.list li'; // 対象のlist
+    $(contents + ':nth-child(n + ' + (show + 1) + ')').addClass('is-hidden');
+    $('.more').on('click', function () {
+      $(contents + '.is-hidden').slice(0, num).removeClass('is-hidden');
+      if ($(contents + '.is-hidden').length == 0) {
+        $('.more').fadeOut();
+      }
+    });
+    
+    
     });
